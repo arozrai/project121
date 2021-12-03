643da4ad28d02da3e57067556cd145e976d58a9e
arozrai <72703446+arozrai@users.noreply.github.com>
2021-12-03 17:40:13 +0000

    Add files via upload

--- project121.py
+++ project121.py
@@ -30,30 +30,30 @@ while (cap.isOpened()):
     #Flipping the image for consistency 
     img = np.flip(img, axis=1)
 
-#  Coverting the color to BGR to HSV
-hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #  Coverting the color to BGR to HSV
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-#  Generating masks to detect color black
-lower_black = np.array([104,153,70])
-upper_black = np.array([30,30,0])
+    #  Generating masks to detect color black
+    lower_black = np.array([104,153,70])
+    upper_black = np.array([30,30,0])
 
-mask = cv2.inRange(hsv,lower_black,upper_black)
+    mask = cv2.inRange(hsv,lower_black,upper_black)
 
-# Providing diluting effect to masks
-mask = cv2.morphologyEx(mask,cv2.MORPH_OPEN, np.ones((3, 3),np.uint8)) 
-mask = cv2.morphologyEx(mask,cv2.MORPH_DILATE, np.ones((3, 3),np.uint8)) 
+    # Providing diluting effect to masks
+    mask = cv2.morphologyEx(mask,cv2.MORPH_OPEN, np.ones((3, 3),np.uint8)) 
+    mask = cv2.morphologyEx(mask,cv2.MORPH_DILATE, np.ones((3, 3),np.uint8)) 
 
-#Keeping only the part of the images without the red color 
-#(or any other color you may choose) 
-res = cv2.bitwise_and(img, img,mask=mask) 
+    #Keeping only the part of the images without the red color 
+    #(or any other color you may choose) 
+    res = cv2.bitwise_and(img, img,mask=mask) 
 
-#Generating the final output by merging res_1 and res_2 
-final_output = cv2.addWeighted(res, 1,0) 
-output_file.write(final_output) 
+    #Generating the final output by merging res_1 and res_2 
+    final_output = cv2.addWeighted(res, 1,0) 
+    output_file.write(final_output) 
 
-#Displaying the output to the user 
-cv2.imshow("magic", final_output) 
-cv2.waitKey(1)
+    #Displaying the output to the user 
+    cv2.imshow("magic", final_output) 
+    cv2.waitKey(1)
 
 cap.release() 
 out.release() 
